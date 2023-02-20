8fdcc999331853a33243d73e1b36e9d7bf166030
nlok5923 <nlokhande5923@gmail.com>
2023-02-20 21:25:49 +0530

    enable message signing

--- banana-wallet-sdk/src/BananaProvider.ts
+++ banana-wallet-sdk/src/BananaProvider.ts
@@ -443,4 +443,8 @@ export class Banana {
       throw err;
     }
   }
+
+  signMessage = async (message: string) => {
+    return await this.bananaSigner.signUserMessage(message, this.cookieObject.encodedId);
+  }
 }
