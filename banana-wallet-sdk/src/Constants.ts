530b75f3c5107619a98e0a317a3415d20343f93a
nlok5923 <nlokhande5923@gmail.com>
2023-02-20 18:09:21 +0530

    polygon: rm redundant statements

--- banana-wallet-sdk/src/Constants.ts
+++ banana-wallet-sdk/src/Constants.ts
@@ -22,17 +22,11 @@ export function getClientConfigInfo(chain: Chains): ClientConfig {
             };
         case Chains.mumbai:
             return {
-                // "entryPointAddress": "0xd5dCACbCa1895347ec142D40a5a3BCf5f72BCc99",
                 "entryPointAddress": "0x8ADd98477E39569e15d807482FFDA67aAd347207",
                 "bundlerUrl": "http://localhost:80/rpc"
             };
         case Chains.optimismTestnet:
             return {
-                // "entryPointAddress": "0x11Ed65bbc8361E0a7869F51Fb9A7FC15bd44a8F6",
-                // "entryPointAddress": "0x1a42Ed84517EB963108668CF3B557d3Ecce52D10",
-                // "entryPointAddress": "0x471EdC1967bffBfe9213083B062F1F27d8C31324",
-                // "entryPointAddress": "0x0576a174D229E3cFA37253523E645A78A0C91B57",
-                // "entryPointAddress": "0x6FE1ee9520cc8F5F1060B267B889a6d66E2ca5f8",
                 "entryPointAddress": "0x17583e7Ff84f336B305D470900C716DCd02627e3",
                 "bundlerUrl": "http://localhost:80/rpc"
             }
@@ -48,7 +42,6 @@ export function getChainSpecificAddress(chain: Chains): ChainConfig {
         };
         case Chains.optimismTestnet:
         return {
-            // "MyWalletDeployer": "0xF9B3f03F2C215eAD853b7A7239746888703989F4", // without signature logic
             "MyWalletDeployer": "0xeccfb3b25378f80Df99da1Bc4d826A6C4c6eC73B",
             "Elliptic": "0x91703a4b78A084B479294634F37A0eA5924D1Ad0"
         };
@@ -59,7 +52,6 @@ export function getChainSpecificAddress(chain: Chains): ChainConfig {
         };
         case Chains.mumbai: 
         return {
-            // "MyWalletDeployer": "0xb0A0Efe6a5b2B03F75F0bF7e2e0EdEee214e9D90",
             "MyWalletDeployer": "0x71CaC8CDf64975A1E936AFC25DF15Fe8b6f8F56B",
             "Elliptic": "0x189c17979aF3cECE12771Dc959254C6DdEc7439b"
         };
