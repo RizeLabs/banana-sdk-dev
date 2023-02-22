4775c38ef05b1a256f303164e8078f7d6e26cc9d
nlok5923 <nlokhande5923@gmail.com>
2023-02-22 21:48:34 +0530

    fix staking

--- banana-wallet-sdk/src/Constants.ts
+++ banana-wallet-sdk/src/Constants.ts
@@ -22,8 +22,9 @@ export function getClientConfigInfo(chain: Chains): ClientConfig {
             };
         case Chains.mumbai:
             return {
-                "entryPointAddress": "0x2167fA17BA3c80Adee05D98F0B55b666Be6829d6",
-                "bundlerUrl": "https://eip4337-banana-bundler.xyz/rpc"
+                // "entryPointAddress": "0xd5dCACbCa1895347ec142D40a5a3BCf5f72BCc99",
+                "entryPointAddress": "0x8ADd98477E39569e15d807482FFDA67aAd347207",
+                "bundlerUrl": "http://localhost:80/rpc"
             };
         case Chains.optimismTestnet:
             return {
@@ -31,7 +32,8 @@ export function getClientConfigInfo(chain: Chains): ClientConfig {
                 // "entryPointAddress": "0x1a42Ed84517EB963108668CF3B557d3Ecce52D10",
                 // "entryPointAddress": "0x471EdC1967bffBfe9213083B062F1F27d8C31324",
                 // "entryPointAddress": "0x0576a174D229E3cFA37253523E645A78A0C91B57",
-                "entryPointAddress": "0x6FE1ee9520cc8F5F1060B267B889a6d66E2ca5f8",
+                // "entryPointAddress": "0x6FE1ee9520cc8F5F1060B267B889a6d66E2ca5f8",
+                "entryPointAddress": "0x17583e7Ff84f336B305D470900C716DCd02627e3",
                 "bundlerUrl": "http://localhost:80/rpc"
             }
     }
@@ -46,8 +48,8 @@ export function getChainSpecificAddress(chain: Chains): ChainConfig {
         };
         case Chains.optimismTestnet:
         return {
-            "MyWalletDeployer": "0xF9B3f03F2C215eAD853b7A7239746888703989F4", // without signature logic
-            // "MyWalletDeployer": "0xeccfb3b25378f80Df99da1Bc4d826A6C4c6eC73B",
+            // "MyWalletDeployer": "0xF9B3f03F2C215eAD853b7A7239746888703989F4", // without signature logic
+            "MyWalletDeployer": "0xeccfb3b25378f80Df99da1Bc4d826A6C4c6eC73B",
             "Elliptic": "0x91703a4b78A084B479294634F37A0eA5924D1Ad0"
         };
         case Chains.matic: 
@@ -57,8 +59,9 @@ export function getChainSpecificAddress(chain: Chains): ChainConfig {
         };
         case Chains.mumbai: 
         return {
-            "MyWalletDeployer": "0xeccfb3b25378f80Df99da1Bc4d826A6C4c6eC73B",
-            "Elliptic": "0x91703a4b78A084B479294634F37A0eA5924D1Ad0"
+            // "MyWalletDeployer": "0xb0A0Efe6a5b2B03F75F0bF7e2e0EdEee214e9D90",
+            "MyWalletDeployer": "0x71CaC8CDf64975A1E936AFC25DF15Fe8b6f8F56B",
+            "Elliptic": "0x189c17979aF3cECE12771Dc959254C6DdEc7439b"
         };
     }
 }
--- example-project/src/pages/Staking/index.jsx
+++ example-project/src/pages/Staking/index.jsx
@@ -15,8 +15,9 @@ const Staking = () => {
   const signerContext = useContext(SignerContext);
   const [showPopover, setShowPopover] = useState(false);
 
+
   // goerli stake address
-  const stakeAddress = '0x1CA35dB18E7f594864b703107FeaE4a24974FCb5'
+  // const stakeAddress = '0x1CA35dB18E7f594864b703107FeaE4a24974FCb5'
 
 
   const signMessage = async () => {
@@ -25,8 +26,11 @@ const Staking = () => {
     const signedMesage = await bananaInstance.signMessage(sampleMsg);
     console.log("Signed message and status: ", signedMesage);
   }
-  const stakeAddress = '0x8b370128A84bc2Df7fF4813675e294b1ae816178'
+  // const stakeAddress = '0x8b370128A84bc2Df7fF4813675e294b1ae816178'
   
+  // polygo staking 
+  const stakeAddress = '0x2144601Dc1b6220F34cf3070Ce8aE5F425aA96F1'
+
   const handleStake = () => {
     setShowPopover(true);
   };
