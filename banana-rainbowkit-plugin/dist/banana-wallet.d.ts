5edfcc107488e212573918deed7da0bc9e26c991
nlok5923 <nlokhande5923@gmail.com>
2023-04-13 19:59:41 +0530

    removing rainbow kit package + example

--- banana-rainbowkit-plugin/dist/banana-wallet.d.ts
+++
@@ -1,8 +0,0 @@
-import { Wallet } from "@rainbow-me/rainbowkit";
-import { Chain } from "wagmi";
-import { ConnectOptions } from "@rize-labs/banana-wallet-sdk-test/dist";
-export interface MyWalletOptions {
-    chains: Chain[];
-    connect?: ConnectOptions;
-}
-export declare const BananaWallet: ({ chains, connect }: MyWalletOptions) => Wallet;
