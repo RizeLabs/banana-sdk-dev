858e6b851340c7b1f63a9820591f9dc334f4175f
nlok5923 <nlokhande5923@gmail.com>
2023-06-07 23:45:16 +0530

    uncomment safe changes

--- banana-contracts/contracts/safe-contracts/Safe.sol
+++ banana-contracts/contracts/safe-contracts/Safe.sol
@@ -102,17 +102,17 @@ contract Safe is
         uint256 payment,
         address payable paymentReceiver
     ) external {
-        // // setupOwners checks if the Threshold is already set, therefore preventing that this method is called twice
-        // setupOwners(_owners, _threshold);
-        // if (fallbackHandler != address(0)) internalSetFallbackHandler(fallbackHandler);
-        // // As setupOwners can only be called if the contract has not been initialized we don't need a check for setupModules
-        // setupModules(to, data);
+        // setupOwners checks if the Threshold is already set, therefore preventing that this method is called twice
+        setupOwners(_owners, _threshold);
+        if (fallbackHandler != address(0)) internalSetFallbackHandler(fallbackHandler);
+        // As setupOwners can only be called if the contract has not been initialized we don't need a check for setupModules
+        setupModules(to, data);
 
-        // if (payment > 0) {
-        //     // To avoid running into issues with EIP-170 we reuse the handlePayment function (to avoid adjusting code of that has been verified we do not adjust the method itself)
-        //     // baseGas = 0, gasPrice = 1 and gas = payment => amount = (payment + 0) * 1 = payment
-        //     handlePayment(payment, 0, 1, paymentToken, paymentReceiver);
-        // }
+        if (payment > 0) {
+            // To avoid running into issues with EIP-170 we reuse the handlePayment function (to avoid adjusting code of that has been verified we do not adjust the method itself)
+            // baseGas = 0, gasPrice = 1 and gas = payment => amount = (payment + 0) * 1 = payment
+            handlePayment(payment, 0, 1, paymentToken, paymentReceiver);
+        }
         emit SafeSetup(msg.sender, _owners, _threshold, to, fallbackHandler);
     }
 
